0ca9ec9586e4d25f73d5c506894c218f2a6255b9
yCels <celso5mf@gmail.com>
2026-03-26 10:04:15 -0300

    feat : estruturando o models

---
+++ escola/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+
+
+class Aluno(models.Model):
+    nome = models.CharField(max_length=30)
+    rg = models.CharField(max_length=30)
+    cpf = models.CharField(max_length=30)
+    data_nasc = models.DateField(max_length=30)
+
+    def __str__(self):
+        return self.nome
+
+
+class Curso(models.Model):
+
+    NIVEL = {       
+        ('B', 'Básico'),
+        ('I', 'Intermediario'),
+        ('A', 'Avançado')
+
+    }
+    codigo_curso = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=20)
+    nível = models.CharField(max_length=40, choices= NIVEL, blank = False, null = False, default='B')  
+
+
+    def __str__(self):
+        return self.descricao
+    
+
+class Matricula(models.Model):
+    PERIODO = (
+        ('M' , 'Matutino'),
+        ('V' , 'Vespertino'),
+        ('N' , 'Noturno')
+    )
+    aluno = models.ForeignKey(Aluno , on_delete = models.CASCADE)
+    curso = models.ForeignKey(Curso , on_delete = models.CASCADE)
+    periodo = models.CharField(max_length  = 1 , choices = PERIODO , blank = False , null = False , default = 'M' )
+    
+    
+
+
+
